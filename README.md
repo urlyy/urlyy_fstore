a5aa8686c110dbccf7d545bbed4cb40e10f39b1a
lyy <2213732736@qq.com>
2024-02-07 16:27:47 +0800

    dotenv->dotenvy

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 编写原因:懒得购买云存储服务，但是想分离出一个单独的文件读写服务，但是又懒得搭minio之类的中间件
 
 - 在配置文件.env中，可以修改服务的ip/port/文件下载接口/文件存储目录路径
-- 通过访问`http://{ip}:{port}/{文件下载接口}/{filename}`来访问该服务所在服务器的本地文件即`./{文件存储目录路径}/{filename}`
-- 通过`POST http://{ip}:{port}/file`上传文件
+- 通过访问`http://{ip}:{port}/{download_route}/{filename}`来访问该服务所在服务器的本地文件即`./{file_path}/{filename}`
+- 通过`POST http://{ip}:{port}/{upload_route}`上传文件
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_files as fs;
-use actix_web::{get, post, App, Error, HttpResponse, HttpServer, Responder};
-use dotenv::dotenv;
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
 use std::env;
 use std::path::{Path, MAIN_SEPARATOR};
 
@@ -16,7 +16,6 @@ struct UploadForm {
     file: TempFile,
 }
 
-#[post("/file")]
 async fn handle_upload(
     MultipartForm(form): MultipartForm<UploadForm>,
 ) -> Result<impl Responder, Error> {
@@ -29,9 +28,9 @@ async fn handle_upload(
     Ok(HttpResponse::Ok())
 }
 
-#[get("/")]
+#[get("/ping")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("pong")
 }
 
 #[actix_web::main]
@@ -40,6 +39,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let file_path = env::var("file_path").unwrap();
     let upload_route = env::var("upload_route").unwrap();
+    let download_route = env::var("download_route").unwrap();
     let ip = env::var("ip").unwrap();
     let port: u16 = env::var("port").unwrap().parse().unwrap();
     std::fs::create_dir_all(&file_path)?;
@@ -47,10 +47,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(TempFileConfig::default().directory(&file_path))
             .service(
-                fs::Files::new(upload_route.as_str(), &file_path), // .show_files_listing()
+                fs::Files::new(download_route.as_str(), &file_path), // .show_files_listing()
             )
+            .route(upload_route.as_str(), web::post().to(handle_upload))
             .service(hello)
-            .service(handle_upload)
     })
     .bind((ip, port))?
     .run()
